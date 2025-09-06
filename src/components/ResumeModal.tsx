dc3c941bb327fe6460a7571b2aa270d7a3067908
sriramragav <srirag@gmail.com>
2025-09-06 08:24:01 +0530

    Update ResumeModal.tsx

--- src/components/ResumeModal.tsx
+++ src/components/ResumeModal.tsx
@@ -10,8 +10,9 @@ interface ResumeModalProps {
 const ResumeModal: React.FC<ResumeModalProps> = ({ open, onOpenChange }) => {
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
-      <DialogContent className="max-w-5xl h-[90vh] p-0">
-        <DialogHeader className="flex justify-between items-center p-4 border-b">
+      <DialogContent className="max-w-5xl h-[90vh] p-0 flex flex-col">
+        {/* Fixed-height header */}
+        <DialogHeader className="flex justify-between items-center p-4 border-b flex-none">
           <DialogTitle className="text-xl font-semibold flex items-center gap-2">
             <FileText className="h-5 w-5" />
             Soumya’s Resume
@@ -20,10 +21,12 @@ const ResumeModal: React.FC<ResumeModalProps> = ({ open, onOpenChange }) => {
             <X className="h-6 w-6 text-gray-600 hover:text-black" />
           </button>
         </DialogHeader>
-        <div className="w-full h-full">
+
+        {/* Flexible body for resume */}
+        <div className="flex-1 overflow-hidden">
           <embed
             src="/assets/SoumyaResume.pdf"
-            title="Soumya Resume"
+            type="application/pdf"
             className="w-full h-full"
           />
         </div>
